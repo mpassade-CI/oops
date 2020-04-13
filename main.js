c2d9d85f545c8ca3fe80c8e3410ac73579262549
Michael Passade <michael.passade@codeimmersives.com>
2020-04-13 19:30:50 -0400

    done

--- main.js
+++ main.js
@@ -1,7 +1,31 @@
 const Stack = function() {
+  return {
+    items: [],
+    add(item){
+      this.items.push(item)
+    },
+    remove(){
+      return this.items.pop()
+    },
+    peek(){
+      return this.items[this.items.length-1]
+    }
+  }
 }
 
 const Queue = function() {
+  return {
+    items: [],
+    add(item){
+      this.items.push(item)
+    },
+    remove(){
+      return this.items.shift()
+    },
+    peek(){
+      return this.items[0]
+    }
+  }
 }
 
 module.exports = {
